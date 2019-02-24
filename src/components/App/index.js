b099b754f7c81f60356ca5109a740276013e0e8d
necheporenko <ne4eporenko.v@gmail.com>
2019-02-24 22:44:25 +0200

    feat: add signup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 // import LandingPage from '../Landing';
-// import SignUpPage from '../SignUp';
+import SignUpPage from '../SignUP';
 // import SignInPage from '../SignIn';
 // import PasswordForgetPage from '../PasswordForget';
 // import HomePage from '../Home';
@@ -19,9 +19,9 @@ const App = () => (
 
       <hr />
 
-      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} /> */}
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      {/* <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
